439ac268079f1052e6c494ec1c82dc0937299638
MaelysCaluri <maelys.caluri@gmail.com>
2025-11-06 00:08:44 +0100

    S2 Q10 en cours

--- Seance-02/src/main.py
+++ Seance-02/src/main.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Mettre dans un commentaire le numéro de la question
@@ -15,14 +16,20 @@ import matplotlib.pyplot as plt
 ## Question 3
 # Notre éditeur est VS_Code
 
+
+
 ## Question 4
 # Source des données : https://www.data.gouv.fr/datasets/election-presidentielle-des-10-et-24-avril-2022-resultats-definitifs-du-1er-tour/
 with open("./data/resultats-elections-presidentielles-2022-1er-tour.csv","r") as fichier:
     contenu = pd.read_csv(fichier)
 
+
+
 ## Question 5
 table = pd.DataFrame(contenu)
 print(table)
+
+
 
 ## Question 6
 nb_ligne   = len(table)
@@ -31,11 +38,45 @@ nb_colonne = len(table.columns)
 reponse = "Le tableau contient {} lignes et {} colonnes."
 print(reponse.format(nb_ligne, nb_colonne))
 
-## Question 7 A FAIIIIIIIIIIIIIIIRE
 
-#  ^
-# /!\ 
-#''T''
+
+## Question 7
+# Pour chaque colonne :
+#   On extrait le premier élément
+#   On regarde son type
+#   On le rajoute à la liste
+liste_types = []
+for nom_colonne in table.columns :
+    element = table[nom_colonne][0]
+    #print(element, " --- ", type(element))
+    if type(element) == str :
+        liste_types.append(str)
+
+    elif type(element) == np.float64 :
+        liste_types.append(float)
+
+    elif type(element) == np.int64 :
+        liste_types.append(int)
+
+    elif type(element) == bool :
+        liste_types.append(bool)
+
+    else :
+        liste_types.append(None)
+
+print(liste_types)  
+
+
 
 ## Question 8
 print(table.head(0))
+
+
+
+## Question 9
+inscrits = table["Inscrits"]
+print(inscrits)
+
+
+
+## Question 10
